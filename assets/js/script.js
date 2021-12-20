0cf7f6e3b0d5ea97f73b52590b7c53fa46b02e3a
EdmonData <96213019+EdmonData@users.noreply.github.com>
2021-12-20 17:58:06 -0300

    trabajando
    detalles finales

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-  $('#contacto').mouseover(function(){
+  $('#contacto').click(function(){
     $('#exampleModalScrollable').modal('show');
   })
+
+   $('.card-body').mouseover(function () {
+    $(this).css('background-color') == 'rgb(23, 162, 184)'
+      ? $(this).css('background-color', 'rgba(197, 90, 90)')
+      : $(this).css('background-color', 'rgb(23, 162, 184)');
+  });
